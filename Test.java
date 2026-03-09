6e29620c164aa8a479769cb43be1f2c93c33cc2b
abebed19 <abebed19@gmail.com>
2026-03-09 10:52:38 +0300

    Try with resources

--- Test.java
+++ Test.java
@@ -5,6 +5,8 @@
  */
 package FlightControl;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -15,16 +17,37 @@ public class Test {
     public static void main(String... args){
         
         Scanner sc = new Scanner(System.in);
-        while (true) {
-        System.out.print("Give a number: ");
-
-        try {
-            int readNumber = Integer.parseInt(sc.nextLine());
-            System.out.println(readNumber);
-        } catch (Exception e) {
-            System.out.println("User input was not a number.");
+        
+        ArrayList<String> demisClan = new ArrayList<>();
+        try(Scanner file = new Scanner(new File("sample.txt"))){
+            
+            while(file.hasNextLine()){
+                demisClan.add(file.nextLine());
+            }
+        }catch(Exception e){
+            
+            
         }
-    }
+        
+        System.out.println(demisClan);
+        
+        
+        
+        while (true) {
+                System.out.print("Give a number: ");
+
+                try {
+                    int readNumber = Integer.parseInt(sc.nextLine());
+                    System.out.println(readNumber);
+                } catch (Exception e) {
+                    System.out.println("User input was not a number.");
+                }
+       } 
+        
+        
+       
+        
+        
    
     }
     
